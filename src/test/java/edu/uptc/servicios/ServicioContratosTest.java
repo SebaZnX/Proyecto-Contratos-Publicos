6ec3c71fb98102e3b8bcaa0ead4cedc2c1eb65c1
JuanJo-02 <juan.rivera22@uptc.edu.co>
2026-06-01 04:19:17 -0500

    Test para Servicio Contratos vacio

--- src/test/java/edu/uptc/servicios/ServicioContratosTest.java
+++ src/test/java/edu/uptc/servicios/ServicioContratosTest.java
@@ -1,4 +1,84 @@
+package edu.uptc.servicios;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ServicioContratosTest {
-  
+
+    @Test
+    void crearContratoPrestacionServicios() {
+    }
+
+    @Test
+    void crearContratoObraPublica() {
+    }
+
+    @Test
+    void crearContratoCompraVenta() {
+    }
+
+    @Test
+    void consultarContrato() {
+    }
+
+    @Test
+    void actualizarContratoGeneral() {
+    }
+
+    @Test
+    void actualizarContratoPrestacionServicios() {
+    }
+
+    @Test
+    void actualizarContratoCompraVenta() {
+    }
+
+    @Test
+    void actualizarContratoObraPublica() {
+    }
+
+    @Test
+    void eliminarContrato() {
+    }
+
+    @Test
+    void agregarContrato() {
+    }
+
+    @Test
+    void asignarContrato() {
+    }
+
+    @Test
+    void seleccionarContrato() {
+    }
+
+    @Test
+    void cambiarEstadoContrato() {
+    }
+
+    @Test
+    void validarContratoPrestacionServicios() {
+    }
+
+    @Test
+    void validarContratoObraPublica() {
+    }
+
+    @Test
+    void existeIdContrato() {
+    }
+
+    @Test
+    void calcularPlazoMeses() {
+    }
+
+    @Test
+    void calcularTotalAdquisicion() {
+    }
+
+    @Test
+    void obtenerTodosLosContratos() {
+    }
 }
