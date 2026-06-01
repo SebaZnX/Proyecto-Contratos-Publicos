a8e5f75525a400967335182456458ab40fcf1e72
JuanJo-02 <juan.rivera22@uptc.edu.co>
2026-06-01 04:18:12 -0500

    Test para Servicio Usuario

--- src/test/java/edu/uptc/servicios/ServicioUsuariosTest.java
+++ src/test/java/edu/uptc/servicios/ServicioUsuariosTest.java
@@ -1,4 +1,72 @@
+package edu.uptc.servicios;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ServicioUsuariosTest {
-  
+
+    @Test
+    void crearAdministrador() {
+    }
+
+    @Test
+    void agregarUsuario() {
+    }
+
+    @Test
+    void loginCorrecto() {
+    }
+
+    @Test
+    void rolLogueado() {
+    }
+
+    @Test
+    void crearContratante() {
+    }
+
+    @Test
+    void consultarContratantes() {
+    }
+
+    @Test
+    void actualizarUsuario() {
+    }
+
+    @Test
+    void actualizarContratante() {
+    }
+
+    @Test
+    void eliminarContratante() {
+    }
+
+    @Test
+    void crearContratista() {
+    }
+
+    @Test
+    void consultarContratistas() {
+    }
+
+    @Test
+    void actualizarContratista() {
+    }
+
+    @Test
+    void mostrarContratistas() {
+    }
+
+    @Test
+    void eliminarContratista() {
+    }
+
+    @Test
+    void numeroDocumentoExiste() {
+    }
+
+    @Test
+    void obtenerUsuario() {
+    }
 }
