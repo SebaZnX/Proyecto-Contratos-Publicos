17f9da2c155446adb5fd97aebca530708e89e482
JuanJo-02 <juan.rivera22@uptc.edu.co>
2026-06-01 04:18:32 -0500

    Test para Servicio Reportes

--- src/test/java/edu/uptc/servicios/ServicioReportesTest.java
+++ src/test/java/edu/uptc/servicios/ServicioReportesTest.java
@@ -1,4 +1,24 @@
+package edu.uptc.servicios;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ServicioReportesTest {
-  
+
+    @Test
+    void guardarReporte() {
+    }
+
+    @Test
+    void mostrarReporte() {
+    }
+
+    @Test
+    void obtenerTodosLosReportes() {
+    }
+
+    @Test
+    void obtenerReportesPorContrato() {
+    }
 }
